b7cbb1259c7d44788a34f99ef5ebbc5bbd45cf4e
Ahmad4433 <aliraza126005#gmail.com>
2024-02-29 21:18:23 +0500

    payment approval page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Hero from "./components/other/hero/Hero";
 import Layout from "./components/layout/Layout";
-import InvoiceQr from "./components/other/invoice-detail-qr/InvoiceQr";
+import ApprovePayment from "./components/other/payments/approve-payment/ApprovePayment";
 
 const App = () => {
 
 
   return (
     <Routes>
-<Route path="/invoive/detail" element={<InvoiceQr />} />
+      <Route path="/payment/approve" element={<ApprovePayment/>} />
       <Route element={<Layout />}>
         <Route path="/" element={<Hero />} />
       </Route>
---
+++ src/components/other/payments/approve-payment/ApprovePayment.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import style from './approvePayment.module.css'
+import { IoCloseSharp } from "react-icons/io5";
+const ApprovePayment = () => {
+  return (
+    <div className={style.main} >
+      <div className={style.card} >
+
+    <div className={style.head} >
+        <p className={style.title} >Pending Approval</p>
+        <IoCloseSharp className={style.icon} />
+    </div>
+    <div className={style.sendDetail} >
+        <p className={style.fromT} >from</p>
+        <p className={style.fromN} >Admin</p>
+    </div>
+    <div className={style.description} >
+        <div className={style.dateS} >
+            <span className={style.dateT} >Date</span>
+            <span className={style.date} >{new Date().toLocaleString()}</span>
+        </div>
+        <div className={style.statusS} >
+            <span className={style.statusT} >status</span>
+            <span className={style.status} >pending...</span>
+        </div>
+    </div>
+    <div className={style.amountS} >
+        <span className={style.amountT} >amount</span>
+        <span className={style.amount} >$55.25</span>
+    </div>
+    <div  className={style.actions} >
+        <button className={style.action} >Approve</button>
+    </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default ApprovePayment
